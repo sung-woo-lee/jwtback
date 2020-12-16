343c43b01f20e38a6be1286e19da8bc4cdde8640
Sung Woo <yashu3lee@gmail.com>
2020-12-16 16:17:18 -0500

    added more stuff

--- server.js
+++ server.js
@@ -1,18 +1,77 @@
 require("dotenv").config();
 
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
 const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const jwtKey = "MY_SECRET_JWT_KEY";
+const jwtExpiry = 60;
 
 const path = require("path");
 
-app.get("/login", (req, res) => {
-  res.sendFile(path.join(`${__dirname}/public/login.html`));
+const users = {
+  user1: "password1",
+  user2: "password2",
+};
+
+const logIn = (req, res) => {
+  const { username, password } = req.body;
+
+  console.log(`Received request, username: ${username}, password: ${password}`);
+  if (!username || !password || users[username] !== password) {
+    // return 401 error is username or password doesn't exist, or if password does
+    // not match the password in our records
+    console.log("bad login attempt");
+    return res
+      .status(400)
+      .json("Login failed. Please check username & password");
+  }
+
+  const token = jwt.sign({ username }, jwtKey, {
+    algorithm: "HS256",
+    expiresIn: jwtExpiry,
+  });
+
+  console.log("token: ", token);
+
+  res.status(201).json({
+    data: {
+      username,
+      password,
+      token: {
+        token,
+        issuedOn: Date.now(),
+        maxAge: jwtExpiry * 1000,
+      },
+    },
+    message: "Login successful! Redirecting now.",
+  });
+};
+
+app.get("/api/login", (req, res) => {
+  console.log("get request received!");
+  res.status(200).json("You did send a get a request!");
 });
+
+app.post(
+  "/api/login",
+  (req, res) => logIn(req, res)
+  // {
+  //   console.log("got request!" + JSON.stringify(req.body));
+  //   res.status(200).json("You did get a request!");
+  // }
+);
 
 app.use("/", (req, res) => {
   res.status(200).send("<h1>HELLO!</h1>");
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, "0.0.0.0", () => {
   console.log(`App listening on port ${process.env.PORT}`);
 });
